64598cebb45e87fee6a701b07e6bc759ab490091
aharon-greenvald <aharongreenvald@gmail.com>
2021-01-10 12:54:33 +0200

    datepiker

--- app/src/main/java/com/example/mybinding/DayViewContainer.kt
+++ app/src/main/java/com/example/mybinding/DayViewContainer.kt
@@ -9,6 +9,7 @@ class DayViewContainer(view: View) : ViewContainer(view) {
     val textView = view.findViewById<TextView>(R.id.calendarDayText)
 
 
+    fun TextView.setTextColor(color: Long) = this.setTextColor(color.toInt())
 
 
 }
--- app/src/main/java/com/example/mybinding/EventActivity.kt
+++ app/src/main/java/com/example/mybinding/EventActivity.kt
@@ -2,10 +2,17 @@ package com.example.mybinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class EventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
